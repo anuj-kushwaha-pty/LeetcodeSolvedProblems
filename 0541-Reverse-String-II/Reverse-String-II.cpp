f8fe1ddfa746403c149839c071f4f6b5160cdae1
ANUJ KUSHWAHA <119887154+anuj-kushwaha-pty@users.noreply.github.com>
2025-07-31 15:04:46 +0530

    [541] [Time Beats: 100.00%] [Memory Beats: 76.29%] - LeetPush

---
+++ 0541-Reverse-String-II/Reverse-String-II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int start = 0, end = min(k, (int)s.length());
+        while(start< s.length()){
+            reverse(s.begin()+start, s.begin()+end);
+            start +=2*k;
+            end = min(start+k, (int)s.length());
+        }
+        return s;
+    }
+};
