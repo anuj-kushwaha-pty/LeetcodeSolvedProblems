f5c3e41bd8199039af03225de1c1f2b3f7f777f1
ANUJ KUSHWAHA <119887154+anuj-kushwaha-pty@users.noreply.github.com>
2025-09-16 11:07:49 +0530

    [3227] [Time Beats: 5.15%] [Memory Beats: 12.75%] - LeetPush

---
+++ 3227-Vowels-Game-in-a-String/Vowels-Game-in-a-String.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        return any_of(begin(s), end(s), [](char ch){
+            return string("aeiou").find(ch) != string::npos;
+        });
+    }
+};
